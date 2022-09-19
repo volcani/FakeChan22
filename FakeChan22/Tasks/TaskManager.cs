5846ab5332cfcc5c24141cc440481b79af98034c
k896951 <git@hgotoh.jp>
2022-09-19 16:35:34 +0900

    テキスト解析と話者選択処理を1カ所に集約

--- FakeChan22/Tasks/TaskManager.cs
+++ FakeChan22/Tasks/TaskManager.cs
@@ -18,8 +18,6 @@ namespace FakeChan22.Tasks
         ListenerConfigClipboard listenerConfigClipboard;
         TaskClipboard taskClipboard;
 
-        SubTaskCommentGen taskCommentGen;
-
         TaskSyncTalk talkTask;
 
         public TaskManager(ref List<ListenerConfig> list, ref MessageQueueWrapper que, ref FakeChanConfig cfg)
@@ -28,6 +26,10 @@ namespace FakeChan22.Tasks
             messQue = que;
             config = cfg;
 
+            // キュー＆発声タスク
+            talkTask = new TaskSyncTalk(ref messQue, ref config);
+
+            // 受信タスク
             foreach (var item in list)
             {
                 switch (item.LsnrType)
@@ -54,11 +56,9 @@ namespace FakeChan22.Tasks
                         break;
                 }
 
-                tasks[item].OnCallAsyncTalk += AsyncTalk;
+                tasks[item].OnCallAsyncTalk += talkTask.AsyncTalk;
             }
 
-            // 常時稼働のタスク
-            talkTask = new TaskSyncTalk(ref messQue, ref config);
         }
 
         public void TaskShutdown()
@@ -95,87 +95,5 @@ namespace FakeChan22.Tasks
             }
         }
 
-        public void AsyncTalk(MessageData talk)
-        {
-            Task.Run(() =>
-            {
-                SpeakerList splist = null;
-                ReplaceDefinitionList replist = null;
-                string sepText = "";
-                string sepMap = "";
-                string parsedText = "";
-                int cid = 0;
-                string cname = "";
-                int splistIndex = -1;
-
-                // 話者のキーとテキストを分離
-                (sepMap, sepText) = ReplaceText.SeparateMapKey(talk.OrgMessage);
-
-                // 非日本語判定の実施判定
-                splist = talk.LsnrCfg.SpeakerListDefault;
-                replist = talk.LsnrCfg.ReplaceListDefault;
-
-                // 非日本語判定の実施判定
-                if (talk.LsnrCfg.IsNoJapanese)
-                {
-                    (bool judge, double rate) = JudgeTextLang.JudgeNoJapanese(talk.OrgMessage, talk.LsnrCfg.NoJapaneseCharRate);
-                    if (judge)
-                    {
-                        splist = talk.LsnrCfg.SpeakerListNoJapaneseJudge;
-                        replist = talk.LsnrCfg.ReplaceListNoJapaneseJudge;
-                    }
-                }
-
-                // 置換処理
-                parsedText = ReplaceText.ParseText(sepText, replist);
-                talk.Message = parsedText;
-
-                // 話者選定
-                if ((sepMap != "") && (splist.SpeakerMaps.ContainsKey(sepMap)))
-                {
-                    // テキスト先頭に話者の識別子が定義されている場合はその話者とする
-                    cid = splist.SpeakerMaps[sepMap].Cid;
-                    cname = splist.SpeakerMaps[sepMap].Name;
-                }
-                else
-                {
-                    if (talk.LsnrCfg.IsRandom)
-                    {
-                        // ランダム指定があれば話者リストの範囲内でランダムに話者を選択する
-                        splistIndex = r.Next(0, splist.ValidSpeakers.Count);
-                        cid = splist.ValidSpeakers[splistIndex].Cid;
-                        cname = splist.ValidSpeakers[splistIndex].Name;
-                    }
-                    else
-                    {
-                        if ((talk.CompatVType != -1) && (talk.CompatVType < splist.ValidSpeakers.Count))
-                        {
-                            // 声質指定がされている場合は話者リスト1番目～9番目に割り当てる
-                            splistIndex = talk.CompatVType;
-                        }
-                        else
-                        {
-                            // 話者リストの範囲を超えていたら話者リスト1番目に割り当てる
-                            splistIndex = 0;
-                        }
-                        cid = splist.ValidSpeakers[splistIndex].Cid;
-                        cname = splist.ValidSpeakers[splistIndex].Name;
-                    }
-                }
-
-                // 発声
-                var eff = splist.ValidSpeakers[splistIndex].Effects.ToDictionary(k => k.ParamName, v => v.Value);
-                var emo = splist.ValidSpeakers[splistIndex].Emotions.ToDictionary(k => k.ParamName, v => v.Value);
-                try
-                {
-                    api.TalkAsync(cid, talk.Message, eff, emo);
-                }
-                catch (Exception)
-                {
-                    //
-                }
-            });
-
-        }
     }
 }
