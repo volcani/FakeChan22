3a052990af2e8f3292357f7fe9536918aae11d82
k896951 <git@hgotoh.jp>
2022-09-29 10:54:05 +0900

    コメント付与と下準備

--- FakeChan22TaskBase/ListenerConfigBase.cs
+++ FakeChan22TaskBase/ListenerConfigBase.cs
@@ -6,36 +6,83 @@ namespace FakeChan22.Tasks
     [DataContract]
     public class ListenerConfigBase
     {
+        /// <summary>
+        /// 名称表示に使われます。
+        /// </summary>
         [DataMember] public string LabelName { get; set; }
 
+        /// <summary>
+        /// 通常時に適用する話者リストです。エディタで変更されます。
+        /// </summary>
         [DataMember] public SpeakerFakeChanList SpeakerListDefault { get; set; }
 
+        /// <summary>
+        /// 内部処理用です。
+        /// </summary>
         [DataMember] public int SpeakerListDefaultIndex { get; set; }
 
+        /// <summary>
+        /// 通常時に適用する置換リストです。エディタで変更されます。
+        /// </summary>
         [DataMember] public ReplaceDefinitionList ReplaceListDefault { get; set; }
 
+        /// <summary>
+        /// 内部処理用です。
+        /// </summary>
         [DataMember] public int ReplaceListDefaultIndex { get; set; }
 
+        /// <summary>
+        /// 非日本語判定の有無です。true時に非日本語判定を行います。エディタで変更されます。
+        /// </summary>
         [DataMember] public bool IsNoJapanese { get; set; }
 
+        /// <summary>
+        /// テキスト中の非日本語文字が、この占有率を越えると非日本語と判定されます。エディタで変更されます。
+        /// </summary>
         [DataMember] public double NoJapaneseCharRate { get; set; }
 
+        /// <summary>
+        /// 非日本語判定時に適用する話者リストです。エディタで変更されます。
+        /// </summary>
         [DataMember] public SpeakerFakeChanList SpeakerListNoJapaneseJudge { get; set; }
 
         [DataMember] public int SpeakerListNoJapaneseJudgeIndex { get; set; }
 
+        /// <summary>
+        /// 非日本語判定時に適用する置換リストです。エディタで変更されます。
+        /// </summary>
         [DataMember] public ReplaceDefinitionList ReplaceListNoJapaneseJudge { get; set; }
 
         [DataMember] public int ReplaceListNoJapaneseJudgeIndex { get; set; }
 
+        /// <summary>
+        /// comment.xmlのService属性に設定する値です。エディタで変更されます。
+        /// </summary>
         [DataMember] public string ServiceName { get; set; }
 
+        /// <summary>
+        /// テキスト(メッセージ)をキューに登録せず非同期に行う場合はtrueにします。エディタで変更されます。
+        /// </summary>
         [DataMember] public bool IsAsync { get; set; }
 
+        /// <summary>
+        /// 話者リスト中の話者をランダムに選択する場合はtrueにします。エディタで変更されます。
+        /// </summary>
         [DataMember] public bool IsRandom { get; set; }
 
+        /// <summary>
+        /// リスナ起動を許可する場合にtrueを設定します。エディタで変更されます。
+        /// </summary>
         [DataMember] public bool IsEnable { get; set; }
 
+        /// <summary>
+        /// テキスト(メッセージ)を置換する前に拡張フィルタを適用する場合にtrueを設定します。エディタで変更されます。
+        /// </summary>
+        [DataMember] public bool IsUseFilter { get; set; }
+
+        /// <summary>
+        /// 内部処理用です。
+        /// </summary>
         [DataMember] public string TaskTypeFullName { get; set; }
 
         public ListenerConfigBase()
@@ -48,6 +95,7 @@ namespace FakeChan22.Tasks
             IsAsync = false;
             IsRandom = false;
             IsEnable = false;
+            IsUseFilter = false;
             NoJapaneseCharRate = 75.0;
         }
     }
